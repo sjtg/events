39fd08a8e9b2bd457831a5d82e8f8667dab90a82
sjtg <chipilej@gmail.com>
2017-10-04 22:52:54 +0200

    1)Created events_detail page that shows each event in more details 2)Added events_details url 3)Created a events_details function in the views.py

--- events/eventsapp/urls.py
+++ events/eventsapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.events_list, name='events_list'),
+	url(r'^events/(?P<pk>\d+)/$', views.events_details, name='events_details'),
 ]
--- events/eventsapp/views.py
+++ events/eventsapp/views.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.utils import timezone
 
 from .models import Events
+
 
 # Create your views here.
 
@@ -15,3 +16,7 @@ from .models import Events
 def events_list(request):
 	events_posts = Events.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'site/events_list.html', {'events_posts' : events_posts})
+
+def events_details(request, pk):
+	events_post = get_object_or_404(Events, pk=pk)
+	return render(request, 'site/events_details.html', {'events_post' : events_post})
