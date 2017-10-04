1f1031e164af13f351cd0b0ab515d609d9e226aa
sjtg <chipilej@gmail.com>
2017-10-04 20:44:15 +0200

    1)Created eventlist webpage (this shows all list of events) 2)Created a event list function inside the views.py

--- events/eventsapp/views.py
+++ events/eventsapp/views.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+
+#created function  to the events list 
+
+def events_list(request):
+	return render(request, 'site/events_list.html', {})
