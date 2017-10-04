6b4a0c62e10d5f02054da9d8ca48b6ea9ea15b2c
sjtg <chipilej@gmail.com>
2017-10-04 21:08:26 +0200

    1)Added events_post variable this will show list of events based when they were created

--- events/eventsapp/views.py
+++ events/eventsapp/views.py
@@ -3,10 +3,15 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.utils import timezone
+
+from .models import Events
+
 # Create your views here.
 
 
 #created function  to the events list 
 
 def events_list(request):
-	return render(request, 'site/events_list.html', {})
+	events_posts = Events.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'site/events_list.html', {'events_posts' : events_posts})
